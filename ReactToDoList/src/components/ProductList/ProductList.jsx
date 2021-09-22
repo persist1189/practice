98a4ee77e16bcf97a1161e5922798e336285ddb4
IvansSu <31758174+IvansSu@users.noreply.github.com>
2021-09-22 21:25:05 +0800

    changeUrl

--- ReactToDoList/src/components/ProductList/ProductList.jsx
+++ ReactToDoList/src/components/ProductList/ProductList.jsx
@@ -5,19 +5,41 @@ import styles from "../../style/_productList.scss"
 const ProductList = () => {
     let history = useHistory();
 
+    const toToDoList = (e) => {
+        setTimeout(() => {
+            e.preventDefault();
+            history.push('toDoList');
+        }, 500);
+    }
+
     const toHome = (e) => {
-        e.preventDefault();
-        history.push('/Home');
+        setTimeout(() => {
+            e.preventDefault();
+            history.push('/Home');
+        }, 500);
     }
     return (
         <div>
             <div className={ styles.container }>
                 <div className={ styles.coverImage }>
-                    <div className={ styles.item }>
-                        <img src="../src/images/fundraising.jpg" alt="fundraising"></img>
-                        <div className={ styles.txt }>
-                            <h2>募款平台：拍出會動的照片</h2>
-                            <p>使用 bootstrap5 + 內建 bootstrap5 javascript 功能。</p>
+                
+                    <a href="https://persist1189.github.io/practice/fundraising/">
+                        <div className={ styles.item }>
+                            <img src="../src/images/fundraising.jpg" alt="fundraising"></img>
+                            <div className={ styles.txt }>
+                                <h2>募款平台：拍出會動的照片</h2>
+                                <p>使用 bootstrap5 + 內建 bootstrap5 javascript 功能。</p>
+                            </div>
+                        </div>    
+                    </a>
+
+                    <div onClick={ toToDoList }>
+                        <div className={ styles.item }>
+                        <img src="../src/images/cutView.png" alt="toDoList"></img>
+                            <div className={ styles.txt }>
+                                <h2>ToDoList</h2>
+                                <p>使用route</p>
+                            </div>
                         </div>
                     </div>
 
@@ -31,28 +53,35 @@ const ProductList = () => {
                         </div>
                     </div>
                     
-                       
-                    <div className={ styles.item }>
-                        <img src="../src/images/blog.jpg" alt="blog"></img>
-                        <div className={ styles.txt }>
-                            <h2>blog 排版</h2>
-                            <p>使用 bootstrap5 功能。</p>
+                    <a href="https://persist1189.github.io/practice/blogTemplate/">
+                        <div className={ styles.item }>
+                            <img src="../src/images/blog.jpg" alt="blog"></img>
+                            <div className={ styles.txt }>
+                                <h2>blog 排版</h2>
+                                <p>使用 bootstrap5 功能。</p>
+                            </div>
                         </div>
-                    </div>
-                    <div className={ styles.item }>
-                        <img src="../src/images/grid.jpg" alt="grid"></img>
-                        <div className={ styles.txt }>
-                            <h2>grid 排版</h2>
-                            <p>使用 bootstrap5 功能。</p>
+                    </a>   
+
+                    <a href="https://persist1189.github.io/practice/gridExample/">
+                        <div className={ styles.item }>
+                            <img src="../src/images/grid.jpg" alt="grid"></img>
+                            <div className={ styles.txt }>
+                                <h2>grid 排版</h2>
+                                <p>使用 bootstrap5 功能。</p>
+                            </div>
+                        </div>                    
+                    </a>
+
+                    <a href="https://persist1189.github.io/practice/rwdAlbum/">
+                        <div className={ styles.item }>
+                            <img src="../src/images/album.jpg" alt="grid"></img>
+                            <div className={ styles.txt }>
+                                <h2>album 排版</h2>
+                                <p>使用 bootstrap5 功能。</p>
+                            </div>
                         </div>
-                    </div>                    
-                    <div className={ styles.item }>
-                        <img src="../src/images/album.jpg" alt="grid"></img>
-                        <div className={ styles.txt }>
-                            <h2>album 排版</h2>
-                            <p>使用 bootstrap5 功能。</p>
-                        </div>
-                    </div>
+                    </a>
                 </div>
             </div>
         </div>
