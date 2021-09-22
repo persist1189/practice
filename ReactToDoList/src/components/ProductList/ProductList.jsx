2d858367593a9cd7a4007da07b31a6c8c3e30607
IvansSu <31758174+IvansSu@users.noreply.github.com>
2021-09-22 18:58:24 +0800

    imageError

--- ReactToDoList/src/components/ProductList/ProductList.jsx
+++ ReactToDoList/src/components/ProductList/ProductList.jsx
@@ -14,7 +14,7 @@ const ProductList = () => {
             <div className={ styles.container }>
                 <div className={ styles.coverImage }>
                     <div className={ styles.item }>
-                        <img src="/src/images/fundraising.jpg" alt="fundraising"></img>
+                        <img src="../src/images/fundraising.jpg" alt="fundraising"></img>
                         <div className={ styles.txt }>
                             <h2>募款平台：拍出會動的照片</h2>
                             <p>使用 bootstrap5 + 內建 bootstrap5 javascript 功能。</p>
@@ -23,7 +23,7 @@ const ProductList = () => {
 
                     <div onClick={ toHome }>
                         <div className={ styles.item }>
-                        <img src="/src/images/cutView.png" alt="grid"></img>
+                        <img src="../src/images/cutView.png" alt="grid"></img>
                             <div className={ styles.txt }>
                                 <h2>根據六角學院的版型來練習</h2>
                                 <p>使用 javascript 操控元素 / flex 排版</p>
@@ -33,21 +33,21 @@ const ProductList = () => {
                     
                        
                     <div className={ styles.item }>
-                        <img src="/src/images/blog.jpg" alt="blog"></img>
+                        <img src="../src/images/blog.jpg" alt="blog"></img>
                         <div className={ styles.txt }>
                             <h2>blog 排版</h2>
                             <p>使用 bootstrap5 功能。</p>
                         </div>
                     </div>
                     <div className={ styles.item }>
-                        <img src="/src/images/grid.jpg" alt="grid"></img>
+                        <img src="../src/images/grid.jpg" alt="grid"></img>
                         <div className={ styles.txt }>
                             <h2>grid 排版</h2>
                             <p>使用 bootstrap5 功能。</p>
                         </div>
                     </div>                    
                     <div className={ styles.item }>
-                        <img src="/src/images/album.jpg" alt="grid"></img>
+                        <img src="../src/images/album.jpg" alt="grid"></img>
                         <div className={ styles.txt }>
                             <h2>album 排版</h2>
                             <p>使用 bootstrap5 功能。</p>
